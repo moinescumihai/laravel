a3bfc00ffe912a28b9903db5cf9bf0b027d225a4
MMF <moinescumihai@bitbucket.org>
2015-01-05 01:14:22 +0200

    something 2

--- app/DB/Models/Model.php
+++ app/DB/Models/Model.php
@@ -5,32 +5,4 @@ use Illuminate\Support\Facades\DB;
 
 abstract class Model extends BaseModel {
 
-    protected static $queryFields;
-    protected static $queryNum;
-    protected static $queryTotal;
-    protected static $commonTableQuery;
-    private static $commonFilters;
-
-    /**
-     *  To be implemented
-     */
-    protected static function fetchByFilters($data){}
-
-    protected static function fetchByFiltersResponse($data){
-        $condition = ' AND (';
-        foreach ($data['columns'] as $column){
-            $condition .= $column['searchable']=='true' ? $column['name'].' LIKE "%'.$data['search']['value'].'%" OR ': '';
-        }
-        return [
-            'data'  =>  DB::select( self::$queryFields.' '.
-                                    self::$commonTableQuery.' '.
-                                    str_replace('OR )', ' ) ', $condition.=')').
-                                    ' ORDER BY '.$data['columns'][$data['order'][0]['column']]['name'].' '.$data['order'][0]['dir'].
-                                    ' LIMIT '.$data['start'].','.$data['length']),
-            'num'   =>  (int) DB::select(self::$queryNum.self::$commonFilters)[0]->cnt,
-            'total' =>  (int) DB::select(self::$queryTotal)[0]->total
-        ]; 
-    }
-
-
 }
